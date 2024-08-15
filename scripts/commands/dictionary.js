d0e0c93f7be4c095caff135a98f16be842872eb2
Nisan || EditZ <mdnisanofficial@gmail.com>
2024-08-15 21:47:59 +0600

    Update dictionary.js

--- scripts/commands/dictionary.js
+++ scripts/commands/dictionary.js
@@ -1,43 +1,29 @@
-//idea by Adrestia Croimoux
-module.exports.config = {
-  name: "dictionary ",
+module.exports = {
+  config: {
+  name: "in",
   version: "1.0.0",
-  permssion: 0,
-  prefix: true,
-  credits: "nayan",
-  description: "",
-  usage: "[text]",
-  category: "study",
-    cooldowns: 5
-}
+  permission: 0,
+  credits: "Nayan",
+  prefix: 'awto',
+  description: "Inbox",
+  category: "without prefix",
+  cooldowns: 5
+},
 
-module.exports.run = function({api,event,args}) {
-  const { threadID, messageID } = event;
-  const fs = require('fs');
-  if (args[0]) {
-    return require('axios').get(encodeURI(`https://api.dictionaryapi.dev/api/v2/entries/en/${args.join(" ").trim().toLowerCase()}`)).then(res => {
-      let data = res.data[0];
-      let example = data.meanings[0].definitions.example;
-      let phonetics = data.phonetics;
-      let meanings = data.meanings
-      msg_meanings = "";
-      meanings.forEach(items => {
-        example = items.definitions[0].example?`\n*example:\n \"${items.definitions[0].example[0].toUpperCase() + items.definitions[0].example.slice(1)}\"`:'';
-        msg_meanings += `\n• ${items.partOfSpeech}\n ${items.definitions[0].definition[0].toUpperCase() + items.definitions[0].definition.slice(1) + example}`
-      });
-      msg_phonetics = '';
-      phonetics.forEach(items => {
-        text = items.text?`\n    /${items.text}/`:'';
-        msg_phonetics += text;
-      })
-      var msg = `❰ ❝ ${data.word} ❞ ❱` +
-                msg_phonetics +
-                msg_meanings;
-      return api.sendMessage(msg, threadID, messageID);
-    }).catch(err => {
-      if (err.response.status === 404) {
-        return api.sendMessage('No Definitions Found', threadID, messageID);
-      }
-    })
-  } else api.sendMessage('Missing input!', threadID, messageID);
+start: async function({ nayan, events, Users, NAYAN }) {
+  let uid;
+  if (events.type === "message_reply") {
+    uid = events.messageReply.senderID;
+  } else if (Object.keys(events.mentions).length > 0) {
+    uid = Object.keys(events.mentions)[0];
+  } else {
+    uid = events.senderID;
+  }
+
+  let name = await Users.getNameUser(uid);
+const msg = `join_my_gc:https://m.me/j/Aba5xjgUn-X7eDrA/
+
+[ ▶️]➜ 𝑵𝒂𝒎𝒆 : ${name}\n[ ▶️]➜ 𝑰𝑫: ${userid}`
+await NAYAN.sendContact(msg, uid, events.threadID, events.messageID);
+}
 }
