fad38187ef1d8bbbfd250e88c5ee5b9d2134a4f1
Nisan || EditZ <mdnisanofficial@gmail.com>
2024-08-15 20:54:18 +0600

    Update chatgpt.js

--- scripts/commands/chatgpt.js
+++ scripts/commands/chatgpt.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-  name: "nayan",
+  name: "nisan",
 	version: "1.0.0",
 	permission: 0,
   prefix: false,
 	credits: "Nayan",
-	description: "I am nayan better than ChatGPT and Bard",
+	description: "I am nisan better than ChatGPT and Bard",
 	category: "user",
 	cooldowns: 5
 };
