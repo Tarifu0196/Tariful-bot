ac1c5f9668cc27b8afd2334496103245b9775862
Nisan || EditZ <mdnisanofficial@gmail.com>
2024-08-15 21:45:07 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["100000959749712"];
+  const permission = ["61556196058001"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
