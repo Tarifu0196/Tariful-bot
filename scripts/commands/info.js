c0448ad36981fde86e4357c54e79292aa6fe28ef
Tarifu0196 <md.tarifulislam0196@gmail.com>
2024-08-17 10:22:54 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,20 +28,20 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝑵𝑰𝑺𝑨𝑵 𝑩𝑩𝒀
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝑵𝑰𝑺𝑨𝑵 𝑯𝑰𝑺𝑺𝑬𝑰𝑵 𝑬𝑫𝑰𝑻𝒁
+𝐍𝐚𝐦𝐞       : TARIFUL ISLAM
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : ভদ্র ছেলে 
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝑭𝒂𝒓𝒊𝒅𝒈𝒐𝒏𝒋, 𝑪𝒉𝒂𝒏𝒅𝒑𝒖𝒓,𝑪𝒉𝒊𝒕𝒕𝒂𝒈𝒐𝒏𝒈
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝑭𝒓𝒊𝒅𝒈𝒐𝒏𝒋, 𝑪𝒉𝒂𝒏𝒅𝒑𝒖𝒓
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-𝐀𝐠𝐞           : 19+
+𝐀𝐠𝐞           : 18+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : nisaneditz@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801775602198
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/nisan_editz198
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/nisan.editz2005`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐆𝐦𝐚𝐢𝐥       : md.tarifulislam0196@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/dewar dorkar nai
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/nai
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/61552422054139`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/61556196058001/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/61552422054139/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
